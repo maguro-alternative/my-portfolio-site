456f0b0c7b18d2ff06456d9370b7762c80076d72
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-11-08 09:16:16 +0900

    conteine移行

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -15,17 +15,6 @@ export default function ProfilePage() {
       className="h-screen overflow-hidden bg-cover"
       style={{ backgroundImage: `url('${selectedBackground}')` }}
     >
-      <div className="flex fixed w-full h-full">
-        <div className="before:absolute after:absolute w-full h-full color-snow text-center before:content-['❄'] before:left-[-30%] before:animate-fall-small-9 after:content-['❄'] after:left-[30%] after:animate-fall-small-9">
-          <span className="absolute animate-fall-small-13">❄</span>
-        </div>
-        <div className="before:absolute after:absolute before:content-['❄'] before:left-[-40%] before:animate-fall-medium-10 after:content-['❄'] after:left-[40%] after:animate-fall-medium-6">
-          <span className="absolute animate-fall-medium-8 left-[10%]">❄</span>
-        </div>
-        <div className="before:absolute after:absolute before:content-['❄'] before:left-[-35%] before:animate-fall-large-6 after:content-['❄'] after:left-[35%] after:animate-fall-large-7">
-          <span className="absolute animate-fall-large-5 left-[-10%]">❄</span>
-        </div>
-      </div>
       <SnowContainer />
     </div>
   );
