f5b8bf6d41ca0d2648ded3d4d9f529f2125e3caa
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-11-21 05:11:55 +0900

    lint

--- src/components/socials/qiita.tsx
+++ src/components/socials/qiita.tsx
@@ -5,40 +5,40 @@ type Props = {
 export default function Qiita({ id }: Props) {
   return (
     <div className='sm:block md:flex lg:flex'>
-              {/*qitta*/}
-              <a 
-                href={`https://qiita.com/${id}`}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <img 
-                  className='sm:float-none md:float-left lg:float-left'
-                  src={`https://badgen.org/img/qiita/${id}/contributions?style=plastic`} 
-                  alt="Contributions" 
-                />
-              </a>
-              <a 
-                href={`https://qiita.com/${id}`}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <img 
-                  className='sm:float-none md:float-left lg:float-left'
-                  src={`https://badgen.org/img/qiita/${id}/followers?style=plastic`} 
-                  alt="Followers" 
-                />
-              </a>
-              <a 
-                href={`https://qiita.com/${id}`}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <img 
-                  className='sm:float-none md:float-left lg:float-left'
-                  src={`https://badgen.org/img/qiita/${id}/articles?style=plastic`} 
-                  alt="Articles"
-                />
-              </a>
-            </div>
+      {/*qitta*/}
+      <a 
+        href={`https://qiita.com/${id}`}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <img 
+          className='sm:float-none md:float-left lg:float-left'
+          src={`https://badgen.org/img/qiita/${id}/contributions?style=plastic`} 
+          alt="Contributions" 
+        />
+      </a>
+      <a 
+        href={`https://qiita.com/${id}`}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <img 
+          className='sm:float-none md:float-left lg:float-left'
+          src={`https://badgen.org/img/qiita/${id}/followers?style=plastic`} 
+          alt="Followers" 
+        />
+      </a>
+      <a 
+        href={`https://qiita.com/${id}`}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <img 
+          className='sm:float-none md:float-left lg:float-left'
+          src={`https://badgen.org/img/qiita/${id}/articles?style=plastic`} 
+          alt="Articles"
+        />
+      </a>
+    </div>
   )
 }
