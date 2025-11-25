bdbafa1b29b62d284df2bb0b46fe9e1701d4e201
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-11-25 09:03:44 +0900

    qiitaのstorybook追加

---
+++ src/components/socials/qiita.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import Qiita from './qiita';
+
+const meta = {
+  title: 'commonUI/Qiita',
+  component: Qiita,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Qiita>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: 'maguro_alterna',
+  },
+};
